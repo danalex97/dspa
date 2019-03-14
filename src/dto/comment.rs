afbf884fa9bbfc91764d4acfc66f293cd1163609
Alexandru Dan <dan.alex97@yahoo.com>
2019-03-14 12:18:22 +0000

    Written a comment DTO.

---
+++ src/dto/comment.rs
@@ -0,0 +1,58 @@
+extern crate chrono;
+extern crate csv;
+
+use chrono::{DateTime, FixedOffset};
+use csv::StringRecord;
+use std::error::Error;
+use std::str::FromStr;
+
+use std::option::Option;
+
+#[derive(Debug)]
+pub struct Comment {
+    pub id: u32,
+    pub person_id: String,
+    pub creation_date: DateTime<FixedOffset>,
+    pub location_ip: std::net::Ipv4Addr,
+    pub browser_used: String,
+    pub content: String,
+    pub reply_to_post_id: Option<u32>,
+    pub reply_to_comment_id: Option<u32>,
+    pub place_id: u32,
+}
+
+fn maybe_record<T>(raw_record: String) -> Option<T> where
+        T: FromStr,
+        <T as std::str::FromStr>::Err: std::fmt::Debug, {
+    if raw_record.is_empty() {
+        None
+    } else {
+        Some(raw_record.parse::<T>().unwrap())
+    }
+}
+
+impl Comment {
+    pub fn from_record(record: StringRecord) -> Result<Comment, Box<Error>> {
+        let id: u32 = record[0].parse()?;
+        let person_id = record[1].parse()?;
+        let creation_date = DateTime::parse_from_rfc3339(&record[2])?;
+        let location_ip = record[3].parse()?;
+        let browser_used = record[4].parse()?;
+        let content = record[5].parse()?;
+        let reply_to_post_id = maybe_record::<u32>(record[6].parse()?);
+        let reply_to_comment_id = maybe_record::<u32>(record[7].parse()?);
+        let place_id = record[8].parse()?;
+
+        Ok(Comment{
+            id,
+            person_id,
+            creation_date,
+            location_ip,
+            browser_used,
+            content,
+            reply_to_post_id,
+            reply_to_comment_id,
+            place_id,
+        })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+mod dto;
+
+use dto::comment::Comment;
+use csv::StringRecord;
+
 fn main() {
-    println!("Hello, world!");
+    let raw_record: &str      = "46228400|9106|2010-02-02T00:06:24Z|31.31.96.17|Firefox|About André Sá, Sá is a professional Brazilian. About Gustav Mahler, performance standards ensured his. About Thomas Aquinas, seminar method. It has school.||46228380|102";
+    let vec_record: Vec<&str> = raw_record.split('|').collect();
+    let c = Comment::from_record(StringRecord::from(vec_record));
+    println!("{:?}", c);
 }
