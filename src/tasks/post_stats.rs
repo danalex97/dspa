6d141f2d09a2dd240dc6e34217b8ee1e7b35b6d8
Alexandru Dan <dan.alex97@yahoo.com>
2019-04-03 18:58:22 +0100

    Finished creating a stream of posts grouped in the thumbling window.

--- src/tasks/post_stats.rs
+++ src/tasks/post_stats.rs
@@ -13,48 +13,74 @@ use timely::dataflow::operators::generic::operator::Operator;
 use timely::dataflow::channels::pact::Pipeline;
 
 const COLLECTION_PERIOD : usize = 1800; // seconds
+const MAX_DELAY : usize = 500; // seconds
 
 pub fn run() {
     timely::execute_from_args(std::env::args(), |worker| {
         worker.dataflow::<usize, _, _>(|scope| {
-            let mut comment_buffer: HashMap<usize, Vec<Comment>> = HashMap::new();
+            let mut posts_buffer: HashMap<usize, Vec<Post>> = HashMap::new();
 
-            scope
-                .kafka_string_source::<Comment>("comments".to_string())
-                .unary_notify(Pipeline, "CommentStats", None, move |input, output, notificator| {
-                    let mut select_first = false;
+            // produces a list of all the new posts together with the timestamp of the
+            // segment that needs to be processed
+            let new_posts = scope
+                .kafka_string_source::<Post>("posts".to_string())
+                .unary_notify(Pipeline, "CollectPosts", None, move |input, output, notificator| {
+                    // note that we know that the timestamp of the first event should be from
+                    // a post, since we can't recive likes or comments without any post
                     let mut scheduled = false;
+                    let mut first = true;
                     let mut vector = Vec::new();
 
-                    input.for_each(|cap, comments| {
+                    input.for_each(|cap, posts| {
                         let time = cap.time().clone();
 
-                        comments.swap(&mut vector);
-                        for comment in vector.drain(..) {
-                            comment_buffer
+                        posts.swap(&mut vector);
+                        for post in vector.drain(..) {
+                            posts_buffer
                                 .entry(time)
                                 .or_insert(vec![])
-                                .push(comment);
+                                .push(post);
                         }
 
                         // just schedule first point at the first received timestamp
                         if !scheduled {
                             scheduled = true;
-                            notificator.notify_at(cap.delayed(&(time + COLLECTION_PERIOD)));
+                            notificator.notify_at(cap.delayed(&(time + MAX_DELAY)));
                         }
                     });
 
                     notificator.for_each(|cap, _, notificator| {
                         let time = cap.time().clone();
 
-                        // process thumbling window
-                        let mut session = output.session(&cap);
-                        session.give(time);
+                        if first {
+                            first = false;
 
-                        notificator.notify_at(cap.delayed(&(time + COLLECTION_PERIOD)));
+                            // find first event
+                            for t in time - MAX_DELAY..time {
+                                if posts_buffer.contains_key(&t) {
+                                    // found first event timestamp, so we periodic notifications
+                                    notificator.notify_at(cap.delayed(&(t + COLLECTION_PERIOD)));
+                                    break;
+                                }
+                            }
+                        } else {
+                            // get all newly created posts in last 30 minutes window
+                            let mut session = output.session(&cap);
+
+                            let mut all_posts = Vec::new();
+                            for t in time - COLLECTION_PERIOD..time {
+                                posts_buffer
+                                    .entry(t)
+                                    .and_modify(|posts| all_posts.extend(posts.drain(..)));
+                            }
+                            session.give((time, all_posts));
+
+                            // schedule next periodic notification
+                            notificator.notify_at(cap.delayed(&(time + COLLECTION_PERIOD)));
+                        }
                     });
-                })
-                .inspect(|x| println!("{:?}", x));
+                });
+                // .inspect(|x| println!("{:?}", x));
 
             // scope.kafka_string_source::<Post>("posts".to_string())
             //     .inspect(|x| println!("{:?}", x));
