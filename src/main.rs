be92095c28871a35b929f773dd3e1b0674e0f940
Alexandru Dan <dan.alex97@yahoo.com>
2019-04-03 11:30:24 +0100

    Added a Kafka string source.

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,10 @@ mod dto;
 mod connection;
 mod stream;
 
-use stream::listen;
 use connection::import::parse_csv;
 use connection::producer::Producer;
 
 fn main() {
     let mut producer = Producer::new("posts".to_string());
     producer.write_file("data/1k-users-sorted/streams/post_event_stream.csv");
-    listen("posts".to_string());
 }
--- src/stream.rs
+++ src/stream.rs
@@ -3,62 +3,54 @@ extern crate rdkafka;
 extern crate kafkaesque;
 
 use timely::dataflow::operators::Inspect;
+use timely::dataflow::scopes::Scope;
+use timely::dataflow::Stream;
+use timely::Data;
 
 use rdkafka::config::ClientConfig;
 use rdkafka::consumer::{Consumer, BaseConsumer, EmptyConsumerContext};
 
-pub fn listen(topic: String) {
-    let mut args = ::std::env::args();
-    args.next();
+trait KafkaSource<G: Scope> {
+    fn kafka_string_source(&self, topic : String) -> Stream<G, String>;
+}
 
-    // Extract Kafka topic.
-    let brokers = "localhost:9092";
+impl<G: Scope<Timestamp=usize>> KafkaSource<G> for G {
+    fn kafka_string_source(&self, topic : String) -> Stream<G, String> {
+        // Extract Kafka topic.
+        let brokers = "localhost:9092";
 
-    // Create Kafka consumer configuration.
-    // Feel free to change parameters here.
-    let mut consumer_config = ClientConfig::new();
-    consumer_config
-        .set("produce.offset.report", "true")
-        .set("auto.offset.reset", "smallest")
-        .set("group.id", "example")
-        .set("enable.auto.commit", "false")
-        .set("enable.partition.eof", "false")
-        .set("auto.offset.reset", "earliest")
-        .set("session.timeout.ms", "6000")
-        .set("bootstrap.servers", &brokers);
+        // Create Kafka consumer configuration.
+        // Feel free to change parameters here.
+        let mut consumer_config = ClientConfig::new();
+        consumer_config
+            .set("produce.offset.report", "true")
+            .set("auto.offset.reset", "smallest")
+            .set("group.id", "example")
+            .set("enable.auto.commit", "false")
+            .set("enable.partition.eof", "false")
+            .set("auto.offset.reset", "earliest")
+            .set("session.timeout.ms", "6000")
+            .set("bootstrap.servers", &brokers);
 
-    timely::execute_from_args(args, move |worker| {
+        // Create a Kafka consumer.
+        let consumer : BaseConsumer<EmptyConsumerContext> = consumer_config.create().expect("Couldn't create consumer");
+        consumer.subscribe(&[&topic]).expect("Failed to subscribe to topic");
 
-        // A dataflow for producing spans.
-        worker.dataflow::<u64,_,_>(|scope| {
+        kafkaesque::source(self, "KafkaStringSource", consumer, |bytes, capability, output| {
 
-            // Create a Kafka consumer.
-            let consumer : BaseConsumer<EmptyConsumerContext> = consumer_config.create().expect("Couldn't create consumer");
-            consumer.subscribe(&[&topic]).expect("Failed to subscribe to topic");
+            // If the bytes are utf8, convert to string and send.
+            if let Ok(text) = std::str::from_utf8(bytes) {
+                output
+                    .session(capability)
+                    .give(text.to_string());
+            }
 
-            let strings =
-            kafkaesque::source(scope, "KafkaStringSource", consumer, |bytes, capability, output| {
+            // We need some rule to advance timestamps ...
+            let time = *capability.time();
+            capability.downgrade(&(time + 1));
 
-                // If the bytes are utf8, convert to string and send.
-                if let Ok(text) = std::str::from_utf8(bytes) {
-                    output
-                        .session(capability)
-                        .give(text.to_string());
-                }
-
-                // We need some rule to advance timestamps ...
-                let time = *capability.time();
-                capability.downgrade(&(time + 1));
-
-                // Indicate that we are not yet done.
-                false
-            });
-
-            strings.inspect(|x| println!("Observed: {:?}", x));
-
-        });
-
-    }).expect("Timely computation failed somehow");
-
-    println!("Hello, world!");
+            // Indicate that we are not yet done.
+            false
+        })
+    }
 }
