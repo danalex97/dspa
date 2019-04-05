b7067aa5fda0ec1a25e87bc3b3eda034972575d1
Matthew Brookes <mbrookes1304@gmail.com>
2019-04-05 14:41:30 +0100

    Count likes

--- src/tasks/post_stats.rs
+++ src/tasks/post_stats.rs
@@ -21,29 +21,9 @@ use self::timely::dataflow::operators::input::Input;
 
 const COLLECTION_PERIOD : usize = 1800; // seconds
 
-fn stash<T>(container : &mut HashMap<usize, Vec<T>>, time : usize, value : T) {
-    container
-        .entry(time)
-        .or_insert(vec![])
-        .push(value);
-}
-
-fn drain_period<T>(container : &mut HashMap<usize, Vec<T>>, t1 : usize, t2 : usize) -> Vec<T> {
-    let mut all = Vec::new();
-    for t in t1..t2 {
-        container
-            .entry(t)
-            .and_modify(|vec| all.extend(vec.drain(..)));
-    }
-    return all;
-}
-
 pub fn run() {
     timely::execute_from_args(std::env::args(), |worker| {
         worker.dataflow::<usize, _, _>(|scope| {
-            let mut comments_buffer: HashMap<usize, Vec<Comment>> = HashMap::new();
-            let mut posts_buffer: HashMap<usize, Vec<Post>> = HashMap::new();
-
             let posts = scope.kafka_string_source::<Post>("posts".to_string());
             let comments = scope.kafka_string_source::<Comment>("comments".to_string());
             let likes = scope.kafka_string_source::<Like>("likes".to_string());
@@ -81,15 +61,15 @@ pub fn run() {
             let mut posts_buffer = HashMap::new();
             let mut likes_count_buffer = HashMap::new();
 
-            let buffered_posts = posts.buffer(Exchange::new(|p: &Post| p.id as u64), FIXED_BOUNDED_DELAY);
-
-            buffered_posts.binary_notify(&counted_likes, Pipeline, Pipeline, "AggregateLikes", None, move |p_input, l_input, output, notificator| {
+            posts.buffer(Exchange::new(|p: &Post| p.id as u64), FIXED_BOUNDED_DELAY)
+                .binary_notify(&counted_likes, Pipeline, Pipeline, "AggregateLikes", None, move |p_input, l_input, output, notificator| {
                     let mut p_data = Vec::new();
                     p_input.for_each(|cap, input| {
                         if output_epoch_start == 0 {
                             // First post received
                             output_epoch_start = cap.time().clone();
                             output_epoch_start_times.push(output_epoch_start);
+                            notificator.notify_at(cap.delayed(&(output_epoch_start + COLLECTION_PERIOD)));
                         }
                         input.swap(&mut p_data);
                         for post in p_data.drain(..) {
@@ -132,8 +112,10 @@ pub fn run() {
                         }
                         if let Some(mut post_likes) = likes_count_buffer.remove(&(cap.time() - COLLECTION_PERIOD)) {
                             for (post, likes) in post_likes.drain() {
-                                let post: &mut Post = all_posts.get_mut(&post).unwrap();
-                                post.likes += likes;
+                                match all_posts.get_mut(&post) {
+                                    None => {/* no-op, probably because data is ordered poorly */},
+                                    Some(post) => post.likes += likes,
+                                }
                             }
                         }
                         let counts: Vec<(u32, u32)> = all_posts.iter().map(|(id, post)| {
@@ -145,84 +127,6 @@ pub fn run() {
 
                 })
                 .inspect_batch(|t, xs| println!("@{}: {:?}", t, xs));
-
-            /*
-            posts
-                .binary_notify(&comments, Pipeline, Pipeline, "CollectComments", None,
-                    move |p_input, c_input, output, notificator| {
-                    // note that we know that the timestamp of the first event should be from
-                    // a post, since we can't recive likes or comments without any post
-                    let mut scheduled = false;
-                    let mut first = true;
-                    let mut p_vector = Vec::new();
-
-                    p_input.for_each(|cap, posts| {
-                        posts.swap(&mut p_vector);
-                        for post in p_vector.drain(..) {
-                            // buffer posts
-                            let time = post.timestamp().clone();
-                            stash(&mut posts_buffer, post.timestamp(), post);
-
-                            // schedule finding the first chronolgical event at first received event
-                            if !scheduled {
-                                scheduled = true;
-                                notificator.notify_at(cap.delayed(&(time + FIXED_BOUNDED_DELAY)));
-                            }
-                        }
-                    });
-
-                    let mut c_vector = Vec::new();
-                    c_input.for_each(|cap, comments| {
-                        // buffer comments
-                        comments.swap(&mut c_vector);
-                        for comment in c_vector.drain(..) {
-                            stash(&mut comments_buffer, comment.timestamp(), comment);
-                        }
-                    });
-
-                    notificator.for_each(|cap, _, notificator| {
-                        let time = cap.time().clone();
-
-                        if first {
-                            first = false;
-
-                            // find first event
-                            for t in time - FIXED_BOUNDED_DELAY..time {
-                                if posts_buffer.contains_key(&t) {
-                                    // found first event timestamp, so we periodic notifications
-                                    notificator.notify_at(cap.delayed(&(t + COLLECTION_PERIOD)));
-                                    break;
-                                }
-                            }
-                        } else {
-                            // get all newly created posts in last 30 minutes window
-                            let mut session = output.session(&cap);
-
-                            // process all posts
-                            for post in drain_period(
-                                &mut posts_buffer, time - COLLECTION_PERIOD, time
-                            ).drain(..) {
-                                // [TODO]: process posts
-                                println!("{:?}", post);
-                            }
-
-                            // process all comments
-                            for comment in drain_period(
-                                &mut comments_buffer, time - COLLECTION_PERIOD, time
-                            ).drain(..) {
-                                // [TODO]: process comments
-                                println!("{:?}", comment);
-                            }
-
-                            // [TODO]: simplify version
-                            session.give(1);
-
-                            // schedule next periodic notification
-                            notificator.notify_at(cap.delayed(&(time + COLLECTION_PERIOD)));
-                        }
-                    });
-                });
-                */
         });
     }).unwrap();
 }
