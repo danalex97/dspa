1e650b4a51de174510b34e0d36765b5240ebbd81
Alexandru Dan <dan.alex97@yahoo.com>
2019-05-20 08:23:37 +0100

    Added tests for stash.

--- src/dsa/stash.rs
+++ src/dsa/stash.rs
@@ -9,10 +9,12 @@ pub trait Stashable<T> {
 }
 
 impl<T: Debug> Stashable<T> for Stash<T> {
+    // Stash at time `time`.
     fn stash(&mut self, time: usize, value: T) {
         self.entry(time).or_insert(vec![]).push(value);
     }
 
+    // Extracts everything on the interval [time_end-length, time_end)
     fn extract(&mut self, length: usize, time_end: usize) -> Vec<T> {
         let mut all = Vec::new();
         for t in time_end - length..time_end {
@@ -23,4 +25,40 @@ impl<T: Debug> Stashable<T> for Stash<T> {
         }
         all
     }
+}
+
+#[test]
+fn test_stash_doesnt_take_last_element() {
+    let mut stash: Stash<u32> = Stash::new();
+
+    stash.stash(0, 1);
+    stash.stash(1, 2);
+    stash.stash(2, 3);
+    stash.stash(3, 4);
+
+    let vec = stash.extract(3, 3);
+    assert!(vec == vec![1, 2, 3]);
+}
+
+#[test]
+fn test_stashed_elements_are_consumed() {
+    let mut stash: Stash<u32> = Stash::new();
+
+    stash.stash(0, 1);
+    stash.stash(1, 2);
+    stash.stash(2, 3);
+    stash.stash(3, 4);
+
+    let vec = stash.extract(3, 3);
+    assert!(vec == vec![1, 2, 3]);
+
+    stash.stash(3, 4);
+    stash.stash(3, 1);
+
+    let vec = stash.extract(3, 3);
+    let vec2: Vec<u32> = vec![];
+    assert!(vec == vec2);
+
+    let vec = stash.extract(1, 4);
+    assert!(vec == vec![4, 4, 1]);
 }
